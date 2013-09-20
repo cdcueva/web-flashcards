bf55beed19483efb2e384b37cd27927e7c92ee4b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 16:09:29 -0500

    closed some tags

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+enable :sessions
+
 #_______GET________#
 
 get '/' do
@@ -19,10 +21,26 @@ end
 
 post '/home' do
 
+  # @email = params[:email]
+  # user = User.authenticate(@email, params[:password])
+  # if user
+  #   # successfully authenticated; set up session and redirect
+  #   session[:user_id] = user.id
+  #   redirect '/'
+  # else
+  #   # an error occurred, re-render the sign-in form, displaying an error
+  #   @error = "Invalid email or password."
+  #   erb :sign_in
+  # end
+
+
   if User.find_by_email(params[:user][:email])
-    @user ||= User.find_by_email(params[:user][:email])   
+    @user ||= User.find_by_email(params[:user][:email])
+    
+    p session[:user_id] = @user.id   
   else
     @user = User.create(params[:user])
+    p session[:user_id] = @user.id
   end
   @deck = Deck.all
   erb :home
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,7 +1,12 @@
 helpers do
 
   def current_user
+    # @user = User.find(1)
     @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 
 end
