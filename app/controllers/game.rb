782e6373596c175fcdee7bf5c34560ad0e529021
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-21 14:25:57 -0500

    most of game logic is there

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,24 +1,40 @@
 ############GET#############
 
-get '/deck/:deck_id/card/:card_id' do
-  @round = current_user.rounds.last
-  @deck = Deck.find(params[:deck_id])
-  @card = @deck.cards.sample
-  if @card.guesses.last.correct
-    redirect to "/deck/#{@deck.id}/card/#{@card.id}"
+get '/deck/:id' do
+  deck = current_user.decks << Deck.find(params[:id])
+  round = current_user.rounds.last
+
+  redirect to "/round/#{round.id}"
+end
+
+get '/round/:id' do
+  round = Round.find(params[:id])
+  card = round.deck.cards.sample
+  
+  if card.guesses.empty?
+    card
+  elsif card.guesses.last.correct
+    redirect to "/round/#{params[:id]}"
+  else
+    card
+  end
+
+  redirect to "/round/#{round.id}/card/#{card.id}"
+end
+
+
+get '/round/:round_id/card/:card_id' do
+  @round = Round.find(params[:round_id])
+  @deck = Deck.find(@round.deck_id)
+  @card = Card.find(params[:card_id])
+  if Guess.last.correct
+    @message = "Correct! The answer was \"#{Card.find(Guess.last.card_id).answer}\""
+  else
+    @message = "Incorrect! The answer was \"#{Card.find(Guess.last.card_id).answer}\""
   end
 
   erb :game
 end
-
-
-get '/deck/:id' do
-  deck = current_user.decks << Deck.find(params[:id])
-
-  redirect to "/deck/#{params[:id]}/card/0"
-end
-
-
 
 
 ############POST############
@@ -27,17 +43,15 @@ end
 post '/round/:round_id/card/:card_id' do
   guess = params[:guess]
   @card = Card.find(params[:card_id])
-  @deck = Round.find(params[:round_id])
+  @round = Round.find(params[:round_id])
 
   if guess == @card.answer
-    @message = "That is correct! Answer: #{@card.answer}"
     Guess.create(correct: true, card_id: params[:card_id],round_id: params[:round_id])
   else
-    @message = "Sorry. The answer is #{@card.answer}"
     Guess.create(correct: false, card_id: params[:card_id],round_id: params[:round_id])
   end
 
-  redirect to "/deck/#{@deck.id}/card/#{@card.id}"
+  redirect to "/round/#{@round.id}"
 
 end
 
